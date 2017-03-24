6d02534ec71e5022434d3a1fdbb92c452924a1d9
Alan Zimmerman <alan.zimm@gmail.com>
2017-03-24 15:56:41 +0200

    Update for current spacemacs (Spacemacs v.0.200.7)

--- packages.el
+++ packages.el
@@ -10,18 +10,44 @@
 ;;
 ;;; License: GPLv3
 
-(defvar liquid-types-packages
+(defconst liquid-types-packages
   '(
     flycheck
     haskell-mode
     button-lock
     pos-tip
-    popup 
+    popup
     thingatpt
     ;; package liquid-typess go here
+    (flycheck-liquid :location local)
+    (liquid-tip      :location local)
     )
-  "List of all packages to install and/or initialize. Built-in packages
-which require an initialization must be listed explicitly in the list.")
+  "The list of Lisp packages required by the liquid-types layer.
+
+Each entry is either:
+
+1. A symbol, which is interpreted as a package to be installed, or
+
+2. A list of the form (PACKAGE KEYS...), where PACKAGE is the
+    name of the package to be installed or loaded, and KEYS are
+    any number of keyword-value-pairs.
+
+    The following keys are accepted:
+
+    - :excluded (t or nil): Prevent the package from being loaded
+      if value is non-nil
+
+    - :location: Specify a custom installation location.
+      The following values are legal:
+
+      - The symbol `elpa' (default) means PACKAGE will be
+        installed using the Emacs package manager.
+
+      - The symbol `local' directs Spacemacs to load the file at
+        `./local/PACKAGE/PACKAGE.el'
+
+      - A list beginning with the symbol `recipe' is a melpa
+        recipe.  See: https://github.com/milkypostman/melpa#recipe-format")
 
 (defun liquid-types/init-button-lock ())
 
@@ -36,3 +62,33 @@ which require an initialization must be listed explicitly in the list.")
 ;; Often the body of an initialize function uses `use-package'
 ;; For more info on `use-package', see readme:
 ;; https://github.com/jwiegley/use-package
+
+
+(defun liquid-types/post-init-flycheck ()
+  (spacemacs/add-flycheck-hook 'haskell-mode))
+
+
+(defun liquid-types/init-flycheck-liquid ()
+  "Initialize my extension"
+  (progn
+    (require 'flycheck-liquid)
+    (add-hook 'haskell-mode-hook
+              '(lambda () (flycheck-select-checker 'haskell-liquid)))
+    (add-hook 'literate-haskell-mode-hook
+              '(lambda () (flycheck-select-checker 'haskell-liquid)))
+    )
+  )
+
+(defun liquid-types/init-liquid-tip ()
+  "Initialize my extension"
+  (progn
+    (require 'flycheck-liquid)
+    (require 'liquid-tip)
+    (add-hook 'haskell-mode-hook
+              '(lambda () (liquid-tip-mode)))
+    ;; (add-hook 'haskell-mode-hook
+    ;;           (lambda () (liquid-tip-init 'ascii)))
+    ;; (add-hook 'literate-haskell-mode-hook
+    ;;           (lambda () (liquid-tip-init 'ascii)))
+    )
+  )
